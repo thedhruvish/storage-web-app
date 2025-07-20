df1de9dbf991a60e1d1c07cb19fd713c802e9b66
thedhruvish <thedhruvish@gmail.com>
2025-07-20 08:41:56 +0530

    change home page dataview

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -10,17 +10,7 @@ import { FileManagerSkeleton } from "@/components/FileManagerSkeleton";
 
 export default function Home() {
   const [viewMode, setViewMode] = React.useState<"grid" | "list">("grid");
-  const [folderData, setFolderData] = React.useState([]);
-  const [fileData, setFileData] = React.useState([]);
-
   const getDirectoryDataHook = useGetAllDirectoryList();
-  React.useEffect(() => {
-    if (getDirectoryDataHook.isSuccess) {
-      console.log(getDirectoryDataHook.data);
-      setFolderData(getDirectoryDataHook.data.data.directories);
-      setFileData(getDirectoryDataHook.data.data.documents);
-    }
-  }, [getDirectoryDataHook.isSuccess]);
 
   const handleFileClick = (file: any) => {
     console.log("File clicked:", file);
@@ -35,14 +25,14 @@ export default function Home() {
       <FileToolbar viewMode={viewMode} onViewModeChange={setViewMode} />
       <div className='flex-1 p-4'>
         <FileGrid
-          files={folderData}
+          files={getDirectoryDataHook.data?.data.directories || []}
           documentType='folder'
           viewMode={viewMode}
           onFileClick={handleFileClick}
         />
         <Separator className='my-4 w-6' />
         <FileGrid
-          files={fileData}
+          files={getDirectoryDataHook.data?.data.documents || []}
           documentType='file'
           viewMode={viewMode}
           onFileClick={handleFileClick}
