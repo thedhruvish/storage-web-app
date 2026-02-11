e3f7ac6a780bf119381cdfa028ec80fc4f5ce134
thedhruvish <thedhruvish@gmail.com>
2026-02-11 15:12:37 +0530

    fix  the workflow

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
+// import { analyzer } from "vite-bundle-analyzer";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
@@ -10,7 +11,19 @@ export default defineConfig({
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    // analyzer(),
   ],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: true,
+    cssMinify: true,
+    terserOptions: {
+      compress: false,
+      mangle: false
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
